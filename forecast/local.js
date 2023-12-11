8e0c1da97d3a1c809aac143d53a279e0d959f693
Eloi Mesa <108403914+elmees21@users.noreply.github.com>
2023-12-11 22:33:25 +0100

    Update local.js

--- forecast/local.js
+++ forecast/local.js
@@ -1,40 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
 
+  function openInfoPopup() {
+
     const userLanguage = navigator.language || navigator.userLanguage;
-  
-    const translations = {
-      "en": {
-        title: "Weather Forecast",
-        airQuality: "Options:",
-        button: 'Change units to "°F"+Info & Privacy'
-      },
-      "es": {
-        title: "Pronóstico del tiempo",
-        airQuality: "Opciones:",
-        button: 'Cambiar las unidades a "°F"+Información y privacidad'
-      },
-      "ca": {
-        title: "Pronòstic del temps",
-        airQuality: "Opcions:",
-        button: 'Cambiar unitats a "°F"+Informació i privadesa'
-      },
-      "it": {
-        title: "Previsioni del tempo",
-        airQuality: "Opzioni:",
-        button: 'Cambia unità in "°F"+Informazioni e privacy'
-      }
+
+    const popupMessages = {
+      "en": 'This widget comes from "https://weatherwidget.org" and the location is obtained by approximating the IP. If you want to know more about how this data is used, please visit the "https://weatherwidget.org" website.',
+      "es": 'Este widget proviene de "https://weatherwidget.org" y la ubicación se obtiene mediante una aproximación de la IP. Si quiere saber más de como se usan estos datos, por favor, visite el sitio web de "https://weatherwidget.org".',
+      "ca": 'Aquest widget prové de "https://weatherwidget.org" i la ubicació s\'obté mitjançant una aproximació de la IP. Si voleu saber més de com es fan servir aquestes dades, si us plau, visiteu el lloc web de "https://weatherwidget.org".',
+      "it": 'Questo widget proviene da "https://weatherwidget.org" e la posizione si ottiene approssimando l\'IP. Se vuoi saperne di più su come vengono utilizzati questi dati, visita il sito web "https://weatherwidget.org".',
     };
-  
-    const titleElement = document.querySelector('h1');
-    const airQualityElement = document.querySelector('h2');
-    const buttons = document.querySelectorAll('button');
-  
-    const translation = translations[userLanguage.toLowerCase()] || translations["en"];
-  
-    titleElement.textContent = translation.title;
-    airQualityElement.textContent = translation.airQuality;
-    buttons.forEach((button, index) => {
-      button.textContent = translation.button.split('+')[index];
-    });
-  });
-  
+
+    const popupMessage = popupMessages[userLanguage.toLowerCase()] || popupMessages["en"];
+
+    alert(popupMessage);
+  }
+
+  const moreInfoButton = document.getElementById("more-info-button");
+
+  if (moreInfoButton) {
+    moreInfoButton.addEventListener("click", openInfoPopup);
+  }
+});
+
+function redirectToFahrenheit() {
+  window.location.href = './fahrenheit/index.html';
+}
+
+function redirectToCelsius() {
+  window.location.href = '../index.html';
+}
